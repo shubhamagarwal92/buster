0cf5209f567c93898095cb940a46b5b951fa3dbd
Shubham Agarwal <shubhamagarwal92@gmail.com>
2023-08-15 22:52:19 -0400

    Embedding generator using openai

--- buster/examples/generate_embeddings.py
+++ buster/examples/generate_embeddings.py
@@ -1,14 +1,50 @@
 import pandas as pd
-
 from buster.documents_manager import DeepLakeDocumentsManager
+import argparse
 
-REQUIRED_COLUMNS = ["url", "title", "content", "source"]
 
-# Read the csv
-df = pd.read_csv("stackoverflow.csv")
+class EmbeddingGenerator:
+    """
+    Class to generate embeddings
+    """
 
-# initialize our vector store from scratch
-dm = DeepLakeDocumentsManager(vector_store_path="deeplake_store", overwrite=True, required_columns=REQUIRED_COLUMNS)
+    def __init__(self, config):
+        self.class_name = "EmbeddingGenerator"
+        self.config = config
+        self.csv_path = config.csv_path
+        self.vector_store_path = config.vector_store_path
 
-# Generate the embeddings for our documents and store them to the deeplake store
-dm.add(df, csv_checkpoint="embeddings.csv")
+    def generate_embeddings(self):
+        # Read the csv
+        df = pd.read_csv(self.csv_path)
+        # Generate the embeddings for our documents and store them in a deeplake format
+        dm = DeepLakeDocumentsManager(vector_store_path=self.vector_store_path, overwrite=True)
+        dm.add(df)
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Argument parser function
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--vector_store_path",
+        default="",
+        help="",
+    )
+    parser.add_argument(
+        "-c",
+        "--csv_path",
+        default="",
+        help="",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    parsed_args = parse_args()
+    generator = EmbeddingGenerator(parsed_args)
+    generator.generate_embeddings()
