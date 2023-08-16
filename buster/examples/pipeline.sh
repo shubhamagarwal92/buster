57438893b9a7e9ccfec09d8ce6da74b70d595400
Shubham Agarwal <shubhamagarwal92@gmail.com>
2023-08-15 23:46:33 -0400

    Chunk clause

--- buster/examples/pipeline.sh
+++ buster/examples/pipeline.sh
@@ -1,9 +1,12 @@
 # shellcheck disable=SC2162
 read -p "Enter the base url you want to scrape (Default: https://docs.mila.quebec/) " BASE_URL
 read -p "Enter the name of the source (Default: sample_source) " SOURCE
+read -p "Do you want to create chunks (1 - Yes, 0 - no), default 1: " DO_CHUNKS
+
 # Default website
 BASE_URL=${BASE_URL:-"https://docs.mila.quebec/"}
 SOURCE=${SOURCE:-"sample_source"}
+DO_CHUNKS=${DO_CHUNKS:-0}
 
 CURRENT_TIME=$(date +"%T")
 echo "Current time : $CURRENT_TIME"
@@ -23,13 +26,16 @@ echo "Docs are parsed in only directory: ${DOCS_DIR}"
 #httrack "$BASE_URL" -O "${RESOURCES_DIR}"/docs_root_dir
 # scrapy-1.0
 
-python create_chunks.py \
---docs_dir "${DOCS_DIR}" \
---base_url "${BASE_URL}" \
---output_csv "${CSV_FILE_PATH}" \
---source "${SOURCE}" \
---filter
-#
+if [ "${DO_CHUNKS}" == 1 ]; then
+#python create_chunks.py \
+#--docs_dir "${DOCS_DIR}" \
+#--base_url "${BASE_URL}" \
+#--output_csv "${CSV_FILE_PATH}" \
+#--source "${SOURCE}" \
+#--filter
+echo "Chunks created"
+fi
+
 python generate_embeddings.py \
 --vector_store_path "${VECTOR_STORE_PATH}" \
 --csv_path "${CSV_FILE_PATH}" \
